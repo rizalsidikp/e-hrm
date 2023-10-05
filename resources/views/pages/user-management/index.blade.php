2e84a775a3c51b80c90eaec345f76d0780f1c186
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2023-10-05 16:23:54 +0700

    add dummy photo

--- resources/views/pages/user-management/index.blade.php
+++ resources/views/pages/user-management/index.blade.php
@@ -78,7 +78,14 @@
                                                 <p class="text-xs font-weight-bold mb-0">{{ $key + 1 }}</p>
                                             </td>
                                             <td>
-                                                <p class="text-xs font-weight-bold mb-0">{{ $user->nama }}</p>
+                                                <div class="d-flex px-2 py-1">
+                                                    <div>
+                                                      <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
+                                                    </div>
+                                                    <div class="d-flex flex-column justify-content-center">
+                                                        <p class="text-xs font-weight-bold mb-0">{{ $user->nama }}</p>
+                                                    </div>
+                                                </div>
                                             </td>
                                             <td>
                                                 <p class="text-xs font-weight-bold mb-0">{{ $user->email }}</p>
